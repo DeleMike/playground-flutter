c1ceadaee6387bb7688bcc90c89de2eadddd7e7c
DeleMike <akindelemichael65@gmail.com>
2021-01-09 15:08:56 +0100

    refactor code for new project

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
 
-import './screens/home_page.dart';
-import './screens/clip_rect.dart';
-import './screens/clip_r_rect.dart';
-import './screens/clip_oval.dart';
-import './screens/clip_path.dart';
 void main() {
   runApp(MyApp());
 }
@@ -20,11 +15,7 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: '/',
       routes: {
-        '/' : (ctx) => MyHomePage(),
-        ClippingRect.routeName : (ctx) => ClippingRect(),
-        ClippingRRect.routeName : (ctx) => ClippingRRect(),
-        ClippingOval.routeName : (ctx) => ClippingOval(),
-        ClippingPath.routeName : (ctx) => ClippingPath(),
+        
       },
       debugShowCheckedModeBanner: false,
     );
