e6cdfdf32acbc256b05e31c510dbd3c619b7d338
DeleMike <akindelemichael65@gmail.com>
2021-02-10 15:29:17 +0100

    add home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './home_screen.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -13,7 +15,8 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(),
+      home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
