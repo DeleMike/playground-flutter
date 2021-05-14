3f26ccd0deddf63d81dc31ec649a8b0537647329
DeleMike <akindelemichael65@gmail.com>
2021-05-14 10:19:23 +0100

    animate flutter logo

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,20 +8,37 @@ void main() {
 class LogoApp extends StatefulWidget {
   @override
   _LogoAppState createState() => _LogoAppState();
-
 }
-  
-class _LogoAppState extends State<LogoApp>{  
+
+class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
+  Animation<double> animation;
+  AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(duration: const Duration(seconds: 2), vsync: this);
+    animation = Tween<double> (begin: 0, end: 300).animate(controller)
+    ..addListener(() {
+      setState(() {
+        
+      });
+    });
+
+    controller.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
-      return Center(
-        child: Container(
-          margin: const EdgeInsets.symmetric(vertical: 10),
-          height: 300,
-          width: 300,
-          child: FlutterLogo(),
-        ),
-      );
+    return Center(
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 10),
+        height: animation.value,
+        width: animation.value,
+        child: FlutterLogo(),
+      ),
+    );
 
     // return MaterialApp(
     //   title: 'Animations with Flutter Logo',
@@ -31,5 +49,10 @@ class _LogoAppState extends State<LogoApp>{
     //    home: MyHomePage(),
     // );
   }
-}
 
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+}
