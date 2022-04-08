2c269f589406662936cdd1a60997d58ee361182e
DeleMike <akindelemichael65@gmail.com>
2022-04-08 16:55:18 +0100

    fix that scrolling issue

--- lib/home.dart
+++ lib/home.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
-  const Home({ Key? key }) : super(key: key);
+  const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(title: Text('Home')),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          physics: ScrollPhysics(),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+             // mainAxisSize: MainAxisSize.min,
+              children: [
+                FlutterLogo(size: 120, style: FlutterLogoStyle.stacked),
+                FlutterLogo(size: 120, style: FlutterLogoStyle.stacked),
+                FlutterLogo(size: 120, style: FlutterLogoStyle.stacked),
+                FlutterLogo(size: 120, style: FlutterLogoStyle.stacked),
+                FlutterLogo(size: 120, style: FlutterLogoStyle.stacked),
+                ListView.builder(
+                  shrinkWrap: true,
+                 physics: NeverScrollableScrollPhysics(),
+                  itemCount: 20,
+                  itemBuilder: ((context, index) => Center(
+                    child: SizedBox(
+                          height: 120,
+                          child: Text('Hello $index 🙂'),
+                        ),
+                  )),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
